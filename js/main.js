b1ca2ae5b690d58c7b04fdfcae6780efec7717ce
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-03 12:20:45 +0100

    test 2

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ let inside=document.querySelector('.square');
 for (let index = 1; index <=100; index++) {
     let element=document.createElement('div');
     element.innerHTML=index;
-    element.addEventListener('click', function(){
+    element.addEventListener('click', Checking) /*function(){
         let number=parseInt(element.innerHTML);
         if(bombs.includes(number)){
             element.classList.add('bomb');
@@ -34,20 +34,58 @@ for (let index = 1; index <=100; index++) {
                 Vittoria(punteggio);
             }
         }
-    });
+    });*/
     inside.append(element);
     element.classList.add('square10');
 }
-//functions
+//functions:
+
+//comportamento in caso di vittoria
 function Vittoria(punti){
     alert('hai vinto');
     document.getElementById('risultato').innerHTML=`hai vinto punti=${punti}`;
+    let hide=document.querySelectorAll('.square10');
+    for (let index = 0; index < hide.length; index++) {
+        hide[index].removeEventListener('click',Checking);
+        
+    }
 }
+//comportamento in caso di sconfitta 
 function Sconfitta(punti){
     alert('hai perso');
     document.getElementById('risultato').innerHTML=`hai perso punti=${punti}`;
-
+    let hide=document.querySelectorAll('.square10');
+    for (let index = 0; index < hide.length; index++) {
+        hide[index].removeEventListener('click',Checking);
+        
+    }
 }
+//impedire all'utente di premere 2 volte la stessa casella
+function RemoveSingleEvent(num){
+    let hide=document.querySelectorAll('.square10');
+    for (let index = 0; index < hide.length; index++) {
+        if(parseInt(hide[index].innerHTML)==num){
+            hide[index].removeEventListener('click',Checking);
+        }    
+    }
+}
+//funzione per la selezione dei numeri
+function Checking(){
+    let number=parseInt(this.innerHTML);
+    if(bombs.includes(number)){
+        this.classList.add('bomb');
+        Sconfitta(punteggio);
+    }else{
+        punteggio+=1;
+        RemoveSingleEvent(number);
+        console.log(punteggio);
+        this.classList.add('checked');
+        if(punteggio==(100-16)){
+            Vittoria(punteggio);
+        }
+    }
+};
+
 //funzione con la difficolta nuova
 function Difficulty(level){
     punteggio=0;
