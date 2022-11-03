44b09c5b24effd46f0cb6457547ab333842495ba
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-03 10:18:40 +0100

    version 1 non completamente ottimizzata

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //default behavior
 //creazione primi 16 numeri
-
+let punteggio=0;
 let bombs=[];
 for (let index = 0; index < 16; index++) {
     let isIn=true
@@ -25,26 +25,63 @@ for (let index = 1; index <=100; index++) {
         let number=parseInt(element.innerHTML);
         if(bombs.includes(number)){
             element.classList.add('bomb');
+            Sconfitta(punteggio);
         }else{
+            punteggio+=1;
             console.log(number);
             element.classList.add('checked');
+            if(punteggio==(100-16)){
+                Vittoria(punteggio);
+            }
         }
     });
     inside.append(element);
     element.classList.add('square10');
 }
-
-
 //functions
+function Vittoria(punti){
+    alert('hai vinto');
+    document.getElementById('risultato').innerHTML=`hai vinto punti=${punti}`;
+}
+function Sconfitta(punti){
+    alert('hai perso');
+    document.getElementById('risultato').innerHTML=`hai perso punti=${punti}`;
+}
+
 function Difficulty(level){
+    punteggio=0;
+    bombs=[]
+    console.log(bombs)
+    for (let index = 0; index < 16; index++) {
+        let isIn=true
+        while(isIn){
+            let randomNum=parseInt(Math.random()*(level-1)+1);
+            if(bombs.includes(randomNum)){
+                isIn=true;
+            }else{
+                bombs.push(randomNum);
+                console.log(randomNum);
+                isIn=false;
+            }   
+        }    
+    }
     inside.innerHTML='';
     for (let index = 1; index <=level; index++) {
         let element=document.createElement('div');
         element.innerHTML=index;
         element.addEventListener('click', function(){
-            let number=element.innerHTML;
+            let number=parseInt(element.innerHTML);
+        if(bombs.includes(number)){
+            element.classList.add('bomb');
+            Sconfitta(punteggio);
+        }else{
+            punteggio+=1;
             console.log(number);
             element.classList.add('checked');
+            if(punteggio==(100-16)){
+                Vittoria(punteggio);
+            }
+        }
         });
         inside.append(element);
         if(level==100){
